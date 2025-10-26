07c3ac1978e8bc064aeb2b9fb1f7f7ffc83710ee
Aakashkalla <aakashpradeepkalla@gmail.com>
2025-10-26 19:51:09 +0530

    Added MongoDB integration and tested CRUD endpoints

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# main.py
-
 from fastapi import FastAPI
 from app.api.endpoints.logs import router as log_router
+from app.core.config import settings
+from app.core.database import db  # your Motor client
 
 app = FastAPI(
     title="Cloud Log Management System – Ingestion, Template Extraction & Compression",
@@ -13,6 +13,20 @@ app = FastAPI(
 )
 
 app.include_router(log_router, prefix="/logs", tags=["Log Pipeline"])
+
+
+@app.on_event("startup")
+async def startup_event():
+    # Print MongoDB URI and database
+    print("🧠 Connected to:", settings.MONGODB_URI)
+    print("🗄️ Using database:", settings.DB_NAME)
+    # Test MongoDB connection
+    try:
+        result = await db.command("ping")
+        print("✅ MongoDB Atlas connection successful:", result)
+    except Exception as e:
+        print("❌ MongoDB connection failed:", e)
+
 
 @app.get("/")
 async def root():
